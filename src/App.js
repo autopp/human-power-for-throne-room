033047fe57d9827b99fbc19c4163b5cc891a4ac9
autopp <autopp.inc@gmail.com>
2018-05-30 09:38:44 +0900

    fix: check # of deck

--- src/App.js
+++ src/App.js
@@ -20,8 +20,8 @@ class App extends Component {
   }
 
   C(x, y) {
-    if (x <= y) {
-      return 1;
+    if (x < y) {
+      return 0;
     }
 
     let r = 1;
@@ -34,11 +34,32 @@ class App extends Component {
   }
 
   calcProbabilityOfAccident() {
-    return 100 * (this.C(this.state.throne + this.state.other, 5) / this.C(this.state.throne + this.state.action + this.state.other, 5) - this.C(this.state.other, 5) / this.C(this.state.throne + this.state.action + this.state.other, 5));
+    let { throne, action, other } = this.state;
+    let total = throne + action + other;
+
+    if (total <= 5) {
+      return throne >= 1 && action == 0 ? 100 : 0;
+    }
+
+    return (this.C(throne + other, 5) - this.C(other, 5)) / this.C(total, 5) * 100;
   }
 
   calcProbabilityOfDoubleThroneAction() {
-    return 100 - (this.C(this.state.action + this.state.other, 5) + this.C(this.state.throne, 1) * this.C(this.state.action + this.state.other, 4) + this.C(this.state.throne, 2) * this.C(this.state.other, 3) + this.C(this.state.throne, 3) * this.C(this.state.other, 2) + this.C(this.state.throne, 4) * this.C(this.state.other, 1) + this.C(this.state.throne, 5)) * 100 / this.C(this.state.throne + this.state.action + this.state.other, 5);
+    let { throne, action, other } = this.state;
+    let total = throne + action + other
+
+    if (total <= 5) {
+      return throne >= 2 && action >= 1 ? 100 : 0;
+    }
+
+    let throne0 = this.C(action + other, 5);
+    let throne1 = this.C(throne, 1) * this.C(action + other, 4);
+    let throne2 = this.C(throne, 2) * this.C(other, 3);
+    let throne3 = this.C(throne, 3) * this.C(other, 2);
+    let throne4 = this.C(throne, 4) * this.C(other, 1);
+    let throne5 = this.C(throne, 5);
+
+    return 100 - (throne0 + throne1 + throne2 + throne3 + throne4 + throne5) / this.C(total, 5) * 100;
   }
 
   render() {
@@ -48,6 +69,8 @@ class App extends Component {
         <div>玉座の間の枚数: <input type="number" value={this.state.throne} min="1" max="10" step="1" onChange={this.onChangeThrone} /></div>
         <div>玉座の間以外のアクションカードの枚数: <input type="number" value={this.state.action} min="1" max="100" step="1" onChange={this.onChangeAction} /></div>
         <div>それ以外のアクションカードの枚数: <input type="number" value={this.state.other} min="0" max="100" step="1" onChange={this.onChangeOther} /></div>
+        <div>玉座事故を引き起こす確率: {this.calcProbabilityOfAccident()}%</div>
+        <div>玉座の間2枚とそれ以外のアクションカードを引ける確率: {this.calcProbabilityOfDoubleThroneAction()}%</div>
       </div>
     );
   }
