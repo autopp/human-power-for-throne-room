93e4fd47bcea2a71dbc90161ffea376b7b2753c4
autopp <autopp.inc@gmail.com>
2018-05-30 09:52:28 +0900

    style: adjust style

--- src/App.js
+++ src/App.js
@@ -60,18 +60,43 @@ class App extends Component {
     let throne4 = this.C(throne, 4) * this.C(other, 1);
     let throne5 = this.C(throne, 5);
 
-    return 100 - (throne0 + throne1 + throne2 + throne3 + throne4 + throne5) / this.C(total, 5) * 100;
+    return Math.abs(100 - (throne0 + throne1 + throne2 + throne3 + throne4 + throne5) / this.C(total, 5) * 100);
   }
 
   render() {
     return (
       <div className="container">
-        <h1>Human power for throne room</h1>
-        <div>玉座の間の枚数: <input type="number" value={this.state.throne} min="1" max="10" step="1" onChange={this.onChangeThrone} /></div>
-        <div>玉座の間以外のアクションカードの枚数: <input type="number" value={this.state.action} min="1" max="100" step="1" onChange={this.onChangeAction} /></div>
-        <div>それ以外のアクションカードの枚数: <input type="number" value={this.state.other} min="0" max="100" step="1" onChange={this.onChangeOther} /></div>
-        <div>玉座事故を引き起こす確率: {sprintf("%.1f", this.calcProbabilityOfAccident())}%</div>
-        <div>玉座の間2枚とそれ以外のアクションカードを引ける確率: {sprintf("%.1f", this.calcProbabilityOfDoubleThroneAction())}%</div>
+        <div className="row">
+          <div className="col-lg-6">
+            <h1>Human power for throne room</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-3">
+            <div>玉座の間の枚数:</div>
+          </div>
+          <div className="col-lg-2">
+            <input type="number" value={this.state.throne} min="1" max="10" step="1" onChange={this.onChangeThrone} />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-3">
+            <div>玉座の間以外のアクションカードの枚数:</div>
+          </div>
+          <div className="col-lg-2">
+            <input type="number" value={this.state.action} min="1" max="100" step="1" onChange={this.onChangeAction} />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-3">
+            <div>それ以外のアクションカードの枚数</div>
+          </div>
+          <div className="col-lg-2">
+            <input type="number" value={this.state.other} min="0" max="100" step="1" onChange={this.onChangeOther} />
+          </div>
+        </div>
+        <h3>玉座事故を引き起こす確率 = {sprintf("%.2f", this.calcProbabilityOfAccident())}%</h3>
+        <h3>玉座の間2枚とそれ以外のアクションカードを引ける確率 = {sprintf("%.2f", this.calcProbabilityOfDoubleThroneAction())}%</h3>
       </div>
     );
   }
